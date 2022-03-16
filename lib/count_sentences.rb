7495d3a448035479ee3efbf8e80f595a6b1bf5f0
Derek Araujo <derekaraujo19@gmail.com>
2022-03-16 15:05:03 -0400

    commit tree 5cc212c4bf909e6278494a8e602f7c9c498435fe

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,31 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sen = self.split(/\.|\?|! /)
+    split_sen.length
+    # binding.pry
+    # acc = 0
+    # punctuation = ['!!', '...','.', '!', '?']
+    # self.chars.each do |char|
+    #   if punctuation.include?(char)
+    #     acc += 1
+    #   end
+    # end
+    # # binding.pry
+    # acc
   end
 end
+
+# "This is a string! It has three sentences. Right?".count_sentences
